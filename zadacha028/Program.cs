e342234c9730376dfa5150c0080be8b641cad78b
Lilia Bezrukova <lilia_dyatlova@mail.ru>
2022-09-14 21:28:58 +0400

    Zadacha 028

---
+++ zadacha028/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход 
+//число N и выдаёт произведение чисел от 1 до N.
+
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Произведение чисел от 1 до {number} равно {FindProductOfNumbers(number)}");
+
+
+
+int FindProductOfNumbers(int number)
+{
+    int mult = 1;
+    int count = 1;
+    while (count <= number)
+    {
+        mult = mult * count;
+        count++;
+    }
+    return mult;
+}
+
