f7374e2f53423600f827449be8c269f28a2a7968
Lilia Bezrukova <lilia_dyatlova@mail.ru>
2022-09-14 20:35:51 +0400

    Zadacha 026

---
+++ zadacha026/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает 
+//на вход число и выдаёт количество цифр в числе.
+
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+
+int count = 0;
+int n = number; // чтобы вывести неизмененый number в ответе
+
+while (n > 0)  
+{
+	int digit = n % 10; // достать последнюю цифру
+	n = n / 10;   		// оставить все, кроме последней цифры
+    count++;
+}
+
+Console.WriteLine($"Количество цифр в числе {number}: {count}");
