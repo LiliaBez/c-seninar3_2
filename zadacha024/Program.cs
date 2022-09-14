5852cc7a9e71637c653d9daf53d66697b1338c59
Lilia Bezrukova <lilia_dyatlova@mail.ru>
2022-09-14 20:09:35 +0400

    Zadacha 024

---
+++ zadacha024/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу, которая принимает на вход
+// число (А) и выдаёт сумму чисел от 1 до А.
+
+Console.WriteLine("Введите N: ");
+int max = int.Parse(Console.ReadLine()!); //число до которого идет суммирование
+
+int sum = 0;
+for (int i = 1; i <= max; i++)
+{
+    sum = sum + i;
+}
+Console.WriteLine(sum);
+
