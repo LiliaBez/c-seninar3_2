f568a4017b83d3c5d2c810b47f96e0f071ad4406
Lilia Bezrukova <lilia_dyatlova@mail.ru>
2022-09-15 13:08:44 +0400

    zadacha030

---
+++ zadacha030/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая выводит массив 
+//из 8 элементов, заполненный нулями и единицами 
+//в случайном порядке.
+
+int[] array = new int[8]; // создать новый массив с 8 элементами
+
+FillArray(array);
+
+void FillArray (int[] array)
+{
+   for (int i = 0; i < array.Length; i++)
+   {
+    array[i] = new Random().Next(0, 2);
+    Console.Write($"{array[i]}, ");
+   }
+   Console.WriteLine();
+}
+
+
+
