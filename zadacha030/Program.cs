4ab74313139f8917f47ab4f19bbc0c2b15e77485
Lilia Bezrukova <lilia_dyatlova@mail.ru>
2022-09-15 13:36:35 +0400

    zadacha030_2

--- zadacha030/Program.cs
+++ zadacha030/Program.cs
@@ -2,19 +2,19 @@
 //из 8 элементов, заполненный нулями и единицами 
 //в случайном порядке.
 
-int[] array = new int[8]; // создать новый массив с 8 элементами
-
-FillArray(array);
-
-void FillArray (int[] array)
+int[] FillArray (int size)
 {
+   int[] array = new int[size]; 
+
    for (int i = 0; i < array.Length; i++)
    {
     array[i] = new Random().Next(0, 2);
-    Console.Write($"{array[i]}, ");
    }
-   Console.WriteLine();
+   return array;
 }
 
+void PrintArray (int[] array){
+    Console.WriteLine($"[{string.Join (", ", array)}]");
+}
 
-
+PrintArray(FillArray(8));
