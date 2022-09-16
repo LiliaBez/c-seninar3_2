0daccc0fd1028fa84ecfe61e7c5ea9e784159017
Lilia Bezrukova <lilia_dyatlova@mail.ru>
2022-09-16 17:56:18 +0400

    Zadacha 025

---
+++ Homework/zadacha025/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Напишите цикл, который принимает на вход 
+два числа (A и B) и возводит число A 
+в натуральную степень B.*/
+
+Console.WriteLine("Введите через запятую числа A и B: ");
+string input = Console.ReadLine();
+int[] seriesNums = Array.ConvertAll(input.Split(','), int.Parse);
+
+Console.WriteLine($"{seriesNums[0]} в степени {seriesNums[1]} = {Exponentiate(seriesNums)}");
+
+
+int Exponentiate (int[] seriesNums) {
+    int mult = seriesNums[0];
+    for (int i = 1; i < seriesNums[1]; i++)
+        mult = mult * seriesNums[0];
+    return mult;
+}
+
+
+
