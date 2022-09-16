a02f69b94662f03e15aeea7e22f1778de97cd3d6
Lilia Bezrukova <lilia_dyatlova@mail.ru>
2022-09-16 21:01:01 +0400

    Zadacha 027

---
+++ Homework/zadacha027/Program.cs
@@ -0,0 +1,18 @@
+﻿/* Напишите программу, которая принимает на вход 
+число и выдаёт сумму цифр в числе.*/
+
+Console.WriteLine("Введите число ");
+int number = int.Parse(Console.ReadLine()!);
+
+int num = number;
+int sum = 0;
+for (int i = 0; num > 0; i++)
+    {
+        int digit = num % 10; 
+	    num = num / 10; 
+        sum = sum + digit;
+    }
+
+Console.WriteLine($"Сумма цифр в числе {number} равняется {sum}");
+
+// ужас... так долго билась, а такая глупая ошибка оказалась
