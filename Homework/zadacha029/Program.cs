01ea62f087be413a7e59eea1fb01a32d7e712363
Lilia Bezrukova <lilia_dyatlova@mail.ru>
2022-09-16 20:35:35 +0400

    Zadacha 029

---
+++ Homework/zadacha029/Program.cs
@@ -0,0 +1,11 @@
+﻿/*Пользователь вводит 8 чисел. 
+Поместите их в массив и распечатайте.*/
+
+Console.WriteLine("Введите через запятую восемь чисел: ");
+
+int[] array = Array.ConvertAll(Console.ReadLine().Split(','), int.Parse);
+
+Console.WriteLine($"[{string.Join (", ", array)}]");
+
+
+// возможно, я не правильно поняла задачу..
